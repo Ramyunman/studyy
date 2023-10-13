73ff0d215e21a10d8e11a02481650406ef18f1ed
Ramyunman <prettyshield@naver.com>
2023-10-13 18:08:23 +0900

    231013-66

---
+++ src/main/java/kr/co/vo/Criteria.java
@@ -0,0 +1,24 @@
+package kr.co.vo;
+
+public class Criteria {
+
+	private int page;
+	private int perPageNum;
+	private int rowStart;
+	private int rowEnd;
+	
+	public Criteria() {
+		this.page = 1;
+		this.perPageNum = 10;
+	}
+	
+	public void setPage(int page) {
+		if (page <= 0) {
+			this.page = 1;
+			return;
+		}
+		this.page = page;
+	}
+	
+	public void 
+}
